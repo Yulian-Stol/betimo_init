626dab643d53aeb32b90c41daca1d57515446445
Yulian <kokomoko12345koko@gmail.com>
2022-11-23 02:17:14 +0200

    first commit

---
+++ js/main/swiper.js
@@ -0,0 +1,14 @@
+
+	var swiper = new Swiper(".mySwiper", {
+		loop: true,
+		effect: 'fade',
+		autoplay: {
+				delay: 2500,
+				disableOnInteraction: false,
+		},
+		fadeEffect: { crossFade: true },
+		pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+		},
+});
